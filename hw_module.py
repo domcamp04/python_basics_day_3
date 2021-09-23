3f08889b1004efb63927ed38ed47c2c26e06a4a4
Dominick Campanile <Dominick@Dominicks-MacBook-Air.local>
2021-09-23 09:22:28 -0500

    finished

---
+++ hw_module.py
@@ -0,0 +1,8 @@
+def sq_ft(l_1, w_1):
+    total_size = l_1 * w_1
+    return total_size
+    print('The total are of your home is ' + (total_size))
+
+def calc_circum(x):
+    circle = 3.14159 * 2 * x
+    print('The circumference of the circle is ' + (circle))
---
+++ new_math/new_math.py
@@ -0,0 +1,7 @@
+def area_calc(x,y):
+    area: x * y
+    print('The square footage of your home is ' + str(area))
+
+def calc_circum(x):
+    circle = 3.14159 * 2 * x
+    print('The circumference of the circle is ' + str(circle))
